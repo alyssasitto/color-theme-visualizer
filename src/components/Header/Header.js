7ec92866e16b6f5385884c6d8684a74676e8452d
Alyssa Sitto <alyssasitto1@gmail.com>
2023-04-16 14:18:07 -0700

    Change heading text in header and theme options sections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,15 @@ const Header = () => {
 	return (
 		<header className="padding-helper">
 			<div>
-				<h1>Find the perfect theme for you</h1>
+				<h1>Find the perfect color scheme for you</h1>
 				<p>
 					With our no-cost platform, you have the ability to discover and
-					experiment with the ideal theme that perfectly suits your website.
+					experiment with the ideal color scheme that perfectly suits your
+					website.
 				</p>
 
 				<Link to="/tester" className="test-theme-btn">
-					Test theme
+					Test color scheme
 				</Link>
 			</div>
 
--- src/components/ThemeOptions/ThemeOptions.js
+++ src/components/ThemeOptions/ThemeOptions.js
@@ -7,7 +7,7 @@ require("./ThemeOptions.css");
 const ThemeOptions = () => {
 	return (
 		<div className="theme-options-container padding-helper">
-			<h2>Don't know which theme to use?</h2>
+			<h2>Don't know which color scheme to use?</h2>
 			<p className="mb-helper themes-p">
 				There are various themes available to choose from depending on the
 				desired mood for your website. Here are a few commonly used ones.
